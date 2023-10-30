56b50b503c3aa4b1820489b78c7509c16e7d1a77
정혜연 <jeonglever@gmail.com>
2023-10-30 19:39:44 +0900

    개발환경 세팅 및 서버 구동 commit

--- src/main/java/com/spring/legacyAndMyBatis/HomeController.java
+++ src/main/java/com/spring/legacyAndMyBatis/HomeController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.SystemPropertyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -25,7 +26,7 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
